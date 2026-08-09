f6cf9a5c49873852369ccb21a88e9f7d871fc8fe
mayankyadav19 <hatmario06@gmail.com>
2026-08-09 17:57:43 +0530

    remove old frontend

--- client/src/components/Chat/TypingIndicator.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function TypingIndicator() {
-  return (
-    <div style={{ padding: '0.5rem 1rem', color: '#666', fontSize: '0.875rem', italic: 'true' }}>
-      <span>AI Interviewer is evaluating your response...</span>
-    </div>
-  );
-}
